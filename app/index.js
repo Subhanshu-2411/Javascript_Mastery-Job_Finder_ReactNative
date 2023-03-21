29f9f3971d175ab27902893888756c05cba67a89
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-03-21 21:10:39 +0530

    Added Home Screen Components

--- app/index.js
+++ app/index.js
@@ -22,10 +22,44 @@ const Home = () => {
                     {
                         headerStyle: {
                             backgroundColor: COLORS.lightWhite
-                        }
+                        },
+                        headerShadowVisible: false,
+                        headerLeft: () => (
+                            <ScreenHeaderBtn
+                                iconUrl={icons.menu} dimension="60%"
+                            />
+                        ),
+                        headerRight: () => (
+                            <ScreenHeaderBtn
+                                iconUrl={icons.profile} dimension="100%"
+                            />
+                        ),
+                        headerTitle: ""
                     }
                 }
             />
+
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+            >
+                <View
+                    style={
+                        {
+                            flex: 1,
+                            padding: SIZES.medium,
+                        },
+                    }
+                >
+                    <Welcome
+
+                    />
+
+                    <Popularjobs/>
+                    <Nearbyjobs/>
+
+                </View>
+
+            </ScrollView>
         </SafeAreaView>
     )
 }
